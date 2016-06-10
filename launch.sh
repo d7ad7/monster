68c594842e95b5c10f54d676bf92c2ff5e21a438
monsterdeev <hasan.muhafada@yahoo.com>
2016-06-10 06:29:57 +0300

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -13,7 +13,7 @@ update() {
 install_luarocks() {
   git clone https://github.com/keplerproject/luarocks.git
   cd luarocks
-  git checkout tags/v2.3.0-rc2 # Release Candidate
+  git checkout tags/v2.2.1 # Current stable
 
   PREFIX="$THIS_DIR/.luarocks"
 
@@ -33,27 +33,12 @@ install_luarocks() {
 }
 
 install_rocks() {
-  ./.luarocks/bin/luarocks install luasec
+  ./.luarocks/bin/luarocks install luasocket
   RET=$?; if [ $RET -ne 0 ];
     then echo "Error. Exiting."; exit $RET;
   fi
 
-  ./.luarocks/bin/luarocks install lbase64 20120807-3
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-
-  ./.luarocks/bin/luarocks install luafilesystem
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-
-  ./.luarocks/bin/luarocks install lub
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-
-  ./.luarocks/bin/luarocks install luaexpat
+  ./.luarocks/bin/luarocks install oauth
   RET=$?; if [ $RET -ne 0 ];
     then echo "Error. Exiting."; exit $RET;
   fi
@@ -69,6 +54,21 @@ install_rocks() {
   fi
 
   ./.luarocks/bin/luarocks install fakeredis
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+
+  ./.luarocks/bin/luarocks install luafilesystem
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+
+  ./.luarocks/bin/luarocks install lub
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+
+  ./.luarocks/bin/luarocks install luaexpat
   RET=$?; if [ $RET -ne 0 ];
     then echo "Error. Exiting."; exit $RET;
   fi
@@ -107,16 +107,6 @@ install() {
   cd ..
   install_luarocks
   install_rocks
-  
-  if [ -d /mnt/c/Windows ]; then
-    echo "Patching bot.lua for Windows 10 support..."
-    sed -i '5d' bot/bot.lua
-    sed -i '5d' bot/bot.lua
-    sed -i '5i\require("bot/utils")' bot/bot.lua
-    sed -i '6i\require("bot/permissions")' bot/bot.lua
-    sed -i '7i\--File patched to support W10' bot/bot.lua
-  fi
-  
 }
 
 if [ "$1" = "install" ]; then
@@ -135,39 +125,6 @@ else
     echo "Run $0 install"
     exit 1
   fi
-  
-  chmod 777 steady.sh
-  chmod 777 start.sh
-  chmod 777 config_fix.sh
-  
-  #Adding some color. By @iicc1 :D
-   echo -e "\033[38;5;208m"
-   echo -e "      ____  ____ _____                        "
-   echo -e "     |    \|  _ )_   _|___ ____   __  __      "
-   echo -e "     | |_  )  _ \ | |/ .__|  _ \_|  \/  |     "
-   echo -e "     |____/|____/ |_|\____/\_____|_/\/\_|     "
-   echo -e "                                              \033[0;00m"
-   echo -e "\e[36m"
-   
-  if [ -f data/config.lua ]; then
-    ./config_fix.sh
-  fi
-  
-  if [ -f plugins/gban_installer.lua ]; then
-    
-    L=$(wc -l plugins/gban_installer.lua | cut -d " " -f1)
-    R=$(echo $L -20 | bc)
-    
-    #N=$(grep -nr "send_msg('chat#id'.*" plugins/gban_installer.lua | cut -d ":" -f1)
-    #M=$(grep -nr "send_msg('channel#id'.*" plugins/gban_installer.lua | cut -d ":" -f1)
-    
-    grep -v "send_msg('chat#id'.*" plugins/gban_installer.lua > gban1
-    grep -v "send_msg('channel#id'.*" gban1 > plugins/gban_installer.lua
-    sed -i "s/.*chat.*/&\n    send_msg('chat#id'..msg.to.id, '$R accounts globally banned. ☠', ok_cb, false)/" plugins/gban_installer.lua
-    sed -i "s/.*channel.*/&\n    send_msg('channel#id'..msg.to.id, '$R accounts globally banned. ☠', ok_cb, false)/" plugins/gban_installer.lua
-    rm gban1
-    
-  fi
-  
+
   ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot/bot.lua -l 1 -E $@
-fi
+fi  
